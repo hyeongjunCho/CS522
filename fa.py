a01d197000395573e3e4199583f5b152468f1529
hyeongjunCho <hyeongjun@kaist.ac.kr>
2017-04-25 06:44:06 +0900

    Implement regex->mDFA, not work

---
+++ fa.py
@@ -0,0 +1,179 @@
+class FA:
+    def __init__(self):
+        self.is_DFA = False
+        self.states = []
+        self.inputs = []
+        self.rule = [] #[q0,input,q1]
+        self.rule_total = [] #[q0][input]->[q1]
+        self.initial = None
+        self.accepts = []
+        self.e_closures = []
+
+
+    def make_rule_total(self):
+        self.rule_total = [["None"] * len(self.inputs) for i in range(len(self.states))]
+        for i in self.rule:
+            if self.rule_total[self.states.index(i[0])][self.inputs.index(i[1])] == "None":
+                self.rule_total[self.states.index(i[0])][self.inputs.index(i[1])] = [i[2]]
+            else:
+                self.rule_total[self.states.index(i[0])][self.inputs.index(i[1])].append(i[2])
+
+
+    def make_e_closure(self):
+        for state in self.states:
+            states = [state]
+            i = 0
+            while i < len(states):
+                e_closure = self.rule_total[self.states.index(states[i])][self.inputs.index("_")]
+
+                if e_closure != "None":
+                    for s in e_closure:
+                        if states.count(s) == 0:
+                            states.append(s)
+                i+=1
+            self.e_closures.append(states)
+
+
+    def to_DFA(self):
+        dfa = FA()
+        dfa.inputs = self.inputs[:]
+        
+        init = self.e_closures[self.states.index(self.initial)]
+
+        dfa.states_dict = {}
+        dfa.states_dict["0"] = init
+        dfa.states.append("0")
+        dfa.initial = "0"
+
+        i = 0
+        while i < len(dfa.states):
+            for input in dfa.inputs:
+                states = []
+                for state in dfa.states_dict[dfa.states[i]]:
+                    temp_states = self.rule_total[self.states.index(state)][self.inputs.index(input)]
+                    if temp_states != "None":
+                        for temp_state in temp_states:
+                            closure = self.e_closures[self.states.index(temp_state)]
+                            for s in closure:
+                                if states.count(s) == 0:
+                                    states.append(s)
+                states.sort(key = lambda x: int(x))
+                #print(states)
+                if states in dfa.states_dict.values():
+                    next_state = list(dfa.states_dict.keys())[list(dfa.states_dict.values()).index(states)]
+                else:
+                    next_state = str(len(dfa.states))
+                    dfa.states_dict[next_state] = states[:]
+                    dfa.states.append(next_state)
+
+                    if dfa.rule.count([dfa.states[i], input, next_state]) == 0:
+                        dfa.rule.append([dfa.states[i], input, next_state])
+                        for s in dfa.states_dict[next_state]:
+                            if self.accepts.count(s) == 1 and dfa.accepts.count(next_state) == 0:
+                                dfa.accepts.append(next_state)
+            i += 1
+            print("i",i,"len",len(dfa.states_dict))
+            '''
+            i += 1
+            if i == len(dfa.states):
+                if count == len(dfa.states) * len(dfa.inputs):
+                    break
+                else:
+                    i = 0
+                    count = 0
+            '''
+        dfa.make_rule_total()
+        for i in dfa.rule_total:
+            for j in i:
+                if j != "None" and len(j) > 2:
+                    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", i)
+        dfa.is_DFA = True
+        print(dfa.states)
+        print(dfa.accepts)
+
+        return dfa
+
+
+    def to_m_DFA(self):
+        if not self.is_DFA:
+            print(1)
+            return (self.to_DFA()).to_m_DFA()
+        mdfa = FA()
+        mdfa.inputs = self.inputs[:]
+        mdfa.states_dict = {}
+        
+        equivalent = [list(set(self.states)-set(self.accepts)), self.accepts]
+
+        while True:
+            l = len(equivalent) # checks for changes of the equivalent
+            flag = 0
+            print("l", l, "len", len(equivalent))
+            print("equivalent", equivalent)
+            for input in mdfa.inputs:
+                for states in equivalent:
+                    temp_states = []
+                    for i in range(len(equivalent)):
+                        temp_states.append(["None"])
+                    for state in states:
+                        temp = self.rule_total[self.states.index(state)][self.inputs.index(input)]
+                        if temp != "None":
+                            temp = temp[0]
+                        print("temp", temp)
+                        for i in range(len(equivalent)):
+                            if equivalent[i].count(temp) == 1:
+                                temp_states[i].append(state)
+                    next_states = []
+                    print("temp_states", temp_states)
+                    for s in temp_states:
+                        if len(s) > 1:
+                            next_states.append(s[1:])
+
+                    if len(next_states) > 1:
+                        flag = 1
+                        break
+
+                if flag == 1:
+                    break
+            print("states", states)
+            print("next_states", next_states)
+
+
+            if flag == 1:
+                equivalent.remove(states)
+                equivalent.extend(next_states)
+                for i in next_states:
+                    states = list(set(states) - set(i))
+                print("deleted states", states)
+                equivalent.append(states)
+                continue
+            
+            if l == len(equivalent):
+                break
+            
+
+        print("self.states", self.states)
+        print("self.rule_total", self.rule_total)
+        print("self.inputs", self.inputs)
+        print("self.accepts", self.accepts)
+        print("equivalent", equivalent)
+        print("len equi", len(equivalent))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- regexlex.py
+++ regexlex.py
@@ -23,7 +23,7 @@ t_RPAREN = r'\)'
 
 
 def t_SYMBOL(t):
-    r'([A-Za-z]+)'
+    r'([A-Za-z0-9_]+)'
     t.value = t.value
     return t
 
@@ -42,18 +42,11 @@ def t_error(t):
 
 lexer = lex.lex()
 data = '''
-( abc * + aa ) . b
+((r+R+s+e+E+f+a+q+Q+t+T+d+w+W+c+z+x+v+g).(k+o+i+O+j+p+u+P+h+h.k+h.o+h.l+y+n+n.j+n.p+n.l+b+m+m.l+l).(_+r+R+r.t+s+s.w+s.g+e+f+f.r+f.a+f.q+f.t+f.x+f.v+f.g+a+q+q.t+t+T+d+w+c+z+x+v+g))*
 '''
+#((1+2+2.a+4+5+5.a+7+8+7.a+7.a.a+1.a+2.a.a+5.a.a+8.a+1.b+2.a.b+5.a.b+7.b+7.a.b)(3+3.9+3.a+3.a.9+3.3+3.3.9+3.3.a+3.3.a.9+6+6.3+6.3.9+6.9+6.a+6.6+6.6.3+6.6.3.9+6.6.9+6.6.a+0+0.9+9)(_+1+1.7+2+2.7.a+2.8.a+2.a+4+4.1+4.5+4.5.a+4.7+4.2.a.a+4.5.a.a+4.8.a+5+5.a+5.a.7+7+8+7.a+7.a.a+1.a+2.a.a+5.a.a+8.a+1.b+7.b))*
+#(0+(1.((0.1)*.(0.0)*.0)*.1)*)*+_
 lexer.input(data)
-
-
-'''
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    print(tok.type, tok.value, tok.lineno, tok.lexpos)
-'''
 
 
 class Node:
@@ -64,47 +57,172 @@ class Node:
 
 '''
 Grammar
-primary -> pi | eps | symbol | ( expression )
+primary -> phi | eps | symbol | ( expression )
 factor -> primary | factor*
 term -> factor | term . factor
 expression -> term | expression + term
 '''
+'''
+regex -> postfix -> nfa
+'''
+
+# ---------------------------------------------
+# regular expression to postfix
+# ---------------------------------------------
+
 class Stack:
     def __init__(self):
         self.symbol_stack = []
-        self.tree = Node()
+        self.postfix_stack = []
+        self.precedence = {'LPAREN':0, 'UNION':1, 'CONCAT':2, 'CLOSURE':3}
     
 
-    def push(self, token):
-        self.symbol_stack.append({type: token.type, value: token.value})
-
-
-    def reduce(self, token = None):
-        if token:
-            pass
+    def read(self, token):
+        if token.type == "SYMBOL":
+            self.postfix_stack.append(token.value)
+        elif token.type == "RPAREN":
+            while True:
+                if self.symbol_stack[-1] == "LPAREN":
+                    self.symbol_stack.pop()
+                    break
+                self.postfix_stack.append(self.symbol_stack.pop())
+        elif token.type == "LPAREN":
+            if self.symbol_stack and self.precedence[self.symbol_stack[-1]] == 3:
+                self.postfix_stack.append(self.symbol_stack.pop())
+            self.symbol_stack.append(token.type)
         else:
-            pass
+            while self.symbol_stack \
+                    and self.precedence[self.symbol_stack[-1]] >= self.precedence[token.type]:
+                self.postfix_stack.append(self.symbol_stack.pop())
+            self.symbol_stack.append(token.type)
+    
+
+    def clear_symbol_stack(self):
+        while self.symbol_stack:
+            self.postfix_stack.append(self.symbol_stack.pop())
 
 
 stack = Stack()
+
 while True:
     tok = lexer.token()
     if not tok:
         break
-    stack.reduce(tok)
-    stack.push(tok)
+    print(tok)
+    stack.read(tok)
+
+stack.clear_symbol_stack()
+
+print("symbol_stack ", stack.symbol_stack)
+print("postfix_stack ", stack.postfix_stack)
+
+postfix = []
+postfix[:] = stack.postfix_stack[:]
+
+binary = ["UNION", "CONCAT"]
+unary = ["CLOSURE"]
+symbols = list(set(postfix) - set(binary) - set(unary))
+del stack
 
 
-stack.reduce()
+# ---------------------------------------------
+# postfix to tree
+# ---------------------------------------------
+
+class Node:
+    def __init__(self, val, children = []):
+        self.val = val
+        if val in symbols:
+            self.type = "SYMBOL"
+        else:
+            self.type = val
+        self.children = children
 
 
+    # test
+    def pref(self):
+        if self:
+            print(self.val, self.children)
+            for i in self.children:
+                pref(i)
 
 
+def post2tree(postfix, unary, binary):
+    stack = []
+
+    for i in postfix:
+        if i in unary:
+            children = [stack.pop()]
+            node = Node(i, children)
+        elif i in binary:
+            children = list(reversed([stack.pop(), stack.pop()]))
+            node = Node(i, children)
+        else:
+            node = Node(i)
+        stack.append(node)
+    return stack[-1]
 
 
+root = post2tree(postfix, unary, binary)
+# pref(root)
 
 
+# ---------------------------------------------
+# tree to nfa
+# ---------------------------------------------
+
+from fa import *
+
+def tree2nfa(root):
+    nfa = FA()
+    nfa.states.append('0')
+    nfa.initial = '0'
+    nfa.inputs = symbols[:]
+
+    def make_nfa(node, final = None):
+        current_state = str(len(nfa.states)-1)
+        if node.type == "UNION":
+            nfa.states.append(str(len(nfa.states)))
+            final = nfa.states[-1]
+            for c in node.children:
+                nfa.states.append(str(len(nfa.states)))
+                nfa.rule.append([current_state, "_", nfa.states[-1]])
+                make_nfa(c, final)
+                nfa.rule.append([nfa.rule[-1][-1], "_", final])
+        elif node.type == "CONCAT":
+            make_nfa(node.children[0])
+            current_state = nfa.rule[-1][-1]
+            nfa.states.append(str(len(nfa.states)))
+            next_state = nfa.states[-1]
+            nfa.rule.append([current_state, "_", next_state])
+            make_nfa(node.children[1])
+        elif node.type == "CLOSURE":
+            nfa.states.append(str(len(nfa.states)))
+            final = nfa.states[-1]
+            nfa.rule.append([current_state, "_", final])
+            nfa.states.append(str(len(nfa.states)))
+            next_state = nfa.states[-1]
+            nfa.rule.append([current_state, "_", next_state])
+            make_nfa(node.children[0])
+            nfa.rule.append([nfa.rule[-1][-1], "_", next_state])
+            nfa.rule.append([nfa.rule[-2][-1], "_", final])
+        else:
+            nfa.states.append(str(len(nfa.states)))
+            nfa.rule.append([current_state, node.val, nfa.states[-1]])
 
 
+    make_nfa(root)
+    print(nfa.states)
+    nfa.make_rule_total()
+    nfa.make_e_closure()
+    nfa.accepts.append(nfa.rule[-1][2])
+    print(len(nfa.states), len(nfa.rule), len(nfa.inputs))
+
+    return nfa
 
 
+nfa = tree2nfa(root)
+print(nfa.accepts)
+dfa = nfa.to_DFA()
+mdfa = dfa.to_m_DFA()
+print(mdfa)
